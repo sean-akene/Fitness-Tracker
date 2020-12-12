f06b1b0720f453a344a846d4b40564712ecc872d
Sean Delaney <seandarren96@gmail.com>
2020-12-11 19:14:13 -0500

    bypassed viewRoute with server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,12 +16,19 @@ app.use(logger("dev"));
 
 //est routes
 app.use(require("./routes/apiRoutes.js"));
-app.use(require("./routes/viewRoutes.js"));
+// app.use(require("./routes/viewRoutes.js"));
+app.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/stats.html'));
+});
+
+app.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/exercise.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
-  });
+});
 
 app.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/stats.html'));
-  });
+});
